3f11f4500a011c1ae2115bf83220543efb591b4b
sofia-frenk <sofia.frenk15@gmail.com>
2023-03-24 16:55:01 -0400

    Added happy

--- HelloWorld.c
+++ HelloWorld.c
@@ -3,7 +3,7 @@
 //-----------------------------------------------------------------------------
 int main(void) {
 
-    printf("Hello Happy World!\n");
+    printf("Hello Happy  World!\n");
 
     return(EXIT_SUCCESS);
 }
